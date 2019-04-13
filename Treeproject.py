4421afa0214b4b4699b3b3e158fc8d22349e4845
AustinSeals <aseals1996@gmail.com>
2019-04-13 19:29:10 -0400

    Male branch of tree added.

--- Treeproject.py
+++ Treeproject.py
@@ -2,19 +2,53 @@
 """
 Created on Sat Apr 13 17:15:49 2019
 
-@author: aseal
+@author: a.seals,p.bruce,s.patel
+
 """
+#Decision Tree implementation 
 
-def edu(eth,gender,status,fr_count,sex,tenure,pop,post_count):
-    if gender == "f" and sex == "straight":
-        if tenure <= 12:
-            if pop == "<50000":
-
-            elif pop == "1mil-5mil":
-                print("bachelors")
-            elif pop == "50000-100000":
-                print("High School")
-    elif tenure <= 9:
-        print("High School")
-    else:
-        print("Masters")
+def edu(eth=str(),gender=str(),status=str(),fr_count=str(),sex=str(),tenure=int(),pop=str(),post_count=int()):
+    if gender =="m" and pop== "<50000" and post_count<=3:
+        print("bachelors")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="black" and status=="single" and sex=="straight" and tenure <=5:
+            print("bachelors")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="black" and status=="single" and sex=="straight" and tenure >5:
+        print("masters")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="black" and status=="single" and sex=="gay":
+        print("bachelors")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="black" and status=="married":
+        print("masters")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="black" and status=="relationship":
+        print("bachelors")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="white" and status=="single":
+        print("high school")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="white" and status=="married":
+        print("masters")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="white" and status=="relationship":
+        print("masters")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="hispanic":
+        print("bachelors")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== ">500"and eth=="asian":
+        print("masters")
+    elif gender =="m" and pop == "<50000" and post_count > 3 and fr_count== "<500":
+        print("bachelors")
+    elif gender =="m" and pop == "1mil-5mil" and sex=="straight":
+        print("masters")
+    elif gender =="m" and pop == "1mil-5mil" and sex=="gay":
+        print("bachelors")
+    elif gender =="m" and pop == "50000-100000" and post_count <=4 and fr_count== ">500":
+        print("highschool")
+    elif gender =="m" and pop == "50000-100000" and post_count > 4 and fr_count== ">500":
+        print("bachelors")
+    elif gender =="m" and pop == "50000-100000" and post_count > 4 and fr_count== "<500":
+        print("bachelors")
+    elif gender =="m" and pop == "500000-1mil":
+        print("masters")
+    elif gender =="m" and pop == "100000-500000" and post_count <= 6 and tenure <= 9:
+        print("masters")
+    elif gender =="m" and pop == "100000-500000" and post_count <= 6 and tenure > 9:
+        print("bachelors")
+    elif gender =="m" and pop == "100000-500000" and post_count <= 6 and tenure <= 9:
+        print("masters")
+    elif gender =="m" and pop == "100000-500000" and post_count >6:
+        print("masters")
