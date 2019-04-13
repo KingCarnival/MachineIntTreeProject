eac4b9342abc5d57c956d3ffeb51243ff760796b
Preston Bruce <pb.itoldyouso@gmail.com>
2019-04-13 17:38:41 -0400

    female tree

--- Treeproject.py
+++ Treeproject.py
@@ -6,4 +6,9 @@ Created on Sat Apr 13 17:15:49 2019
 """
 
 def edu(eth,gender,status,fr_count,sex,tenure,pop,post_count):
-    
+    if gender == "f" and sex == "straight":
+
+    elif tenure <= 9:
+        print("High School")
+    else
+        print("Masters")
